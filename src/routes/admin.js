91e3d8c5c24035188254db24b5e12c53755ba13e
Luan Lopes <luanglopes@gmail.com>
2022-10-20 20:21:21 -0300

    feat: create endpoint to return best clients

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -22,4 +22,23 @@ adminRouter.get('/best-profession', async (req, res) => {
     res.json({ profession })
 })
 
+adminRouter.get('/best-clients', async (req, res) => {
+    const { start, end, limit = 2 } = req.query
+    const sequelize = req.app.get('sequelize')
+
+    const startDate = new Date(start)
+    const endDate = new Date(end)
+
+    const [result] = await sequelize.query('SELECT p.id AS id, p.firstName || p.lastName AS fullName, sum(j.price) AS paid FROM Profiles p JOIN Contracts c ON c.ClientId = p.id JOIN Jobs j ON j.ContractId = c.id WHERE j.paid = :paid AND (j.createdAt BETWEEN :startDate AND :endDate) GROUP BY p.id ORDER BY paid DESC LIMIT :limit', {
+        replacements: {
+            paid: true,
+            startDate,
+            endDate,
+            limit
+        }
+    })
+
+    res.json(result)
+})
+
 module.exports = adminRouter
